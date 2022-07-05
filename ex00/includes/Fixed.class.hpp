6378bd6079417810f51e4962a3dd9f61474f269c
Administrateur <maeva.albrand@icloud.com>
2022-07-05 16:31:30 +0200

    ex00

---
+++ ex00/includes/Fixed.class.hpp
@@ -0,0 +1,19 @@
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+# include <iostream>
+
+class Fixed
+{
+	private :
+		int			_vf;
+		static const int	_bit = 8;
+	public :
+		Fixed(void);
+		Fixed(const Fixed &fix);
+		~Fixed(void);
+		void	operator=(const Fixed &fix); 
+		int	getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+#endif
---
+++ ex00/src/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.class.hpp"
+
+Fixed::Fixed(void) : _vf(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ; 
+}
+
+Fixed::Fixed(const Fixed &fix)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fix;
+	return ; 
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ; 
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_vf = raw;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_vf);
+}
+
+void	Fixed::operator=(const Fixed &fix)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fix)
+		this->_vf = fix.getRawBits();
+}
